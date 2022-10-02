5452da6c625139a36f3f17cac8ea928d9dea1ba6
Linkda52 <67817916+Linkda52@users.noreply.github.com>
2022-10-02 15:24:26 -0400

    UI improvement

---
+++ app/src/main/java/com/cse5236/buckeyeschedule/activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.cse5236.buckeyeschedule.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cse5236.buckeyeschedule.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
