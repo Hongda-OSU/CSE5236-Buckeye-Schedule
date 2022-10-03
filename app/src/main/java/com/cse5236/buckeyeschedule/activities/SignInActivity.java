5b3a6c595ae07b9e4e38b9189b209186962ece5c
Linkda52 <67817916+Linkda52@users.noreply.github.com>
2022-10-03 04:24:21 -0400

    user sign up with firebase

--- app/src/main/java/com/cse5236/buckeyeschedule/activities/SignInActivity.java
+++ app/src/main/java/com/cse5236/buckeyeschedule/activities/SignInActivity.java
@@ -2,9 +2,8 @@ package com.cse5236.buckeyeschedule.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.cse5236.buckeyeschedule.R;
 import com.cse5236.buckeyeschedule.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
@@ -14,8 +13,16 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
     }
+
+    private void setListeners() {
+        binding.signUpNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+
 
     @Override
     protected void onDestroy(){
