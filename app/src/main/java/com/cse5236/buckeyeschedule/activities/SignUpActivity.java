2cac25a087b564a9343e2b75410059a7c78afd24
Linkda52 <67817916+Linkda52@users.noreply.github.com>
2022-10-05 05:53:36 -0400

    sign in finished

--- app/src/main/java/com/cse5236/buckeyeschedule/activities/SignUpActivity.java
+++ app/src/main/java/com/cse5236/buckeyeschedule/activities/SignUpActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
@@ -39,6 +40,39 @@ public class SignUpActivity extends AppCompatActivity {
         preferenceManager = new PreferenceManager(getApplicationContext());
         setContentView(binding.getRoot());
         setListeners();
+        Log.d("lifecycle","onCreate invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
     }
 
     private void setListeners() {
@@ -150,10 +184,5 @@ public class SignUpActivity extends AppCompatActivity {
             binding.buttonSignUp.setVisibility(View.VISIBLE);
             binding.progressBar.setVisibility(View.INVISIBLE);
         }
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
     }
 }
