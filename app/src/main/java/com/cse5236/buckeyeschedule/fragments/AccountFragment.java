d9067a7369d4b35e3ff5780a9ea9e75d690364df
Linkda52 <linhongda77@gmail.com>
2022-10-20 01:47:54 -0400

    button and alart for update account and delete account, working on logic

--- app/src/main/java/com/cse5236/buckeyeschedule/fragments/AccountFragment.java
+++ app/src/main/java/com/cse5236/buckeyeschedule/fragments/AccountFragment.java
@@ -1,5 +1,7 @@
 package com.cse5236.buckeyeschedule.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.cse5236.buckeyeschedule.activities.MainActivity;
 import com.cse5236.buckeyeschedule.databinding.FragmentAccountBinding;
@@ -23,6 +26,9 @@ public class AccountFragment extends Fragment {
 
     private FragmentAccountBinding binding;
     private PreferenceManager preferenceManager;
+    private DocumentSnapshot documentSnapshot;
+    private AlertDialog dialog;
+    private AlertDialog.Builder builder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +36,7 @@ public class AccountFragment extends Fragment {
         ((MainActivity)getActivity()).setTitle("Account Setting");
         ((MainActivity)getActivity()).miniIconHelper(false);
         binding = FragmentAccountBinding.inflate(inflater, container, false);
+        builder = new AlertDialog.Builder(getActivity());
         View v = binding.getRoot();
         preferenceManager = new PreferenceManager(getActivity().getApplicationContext());
         //Toast.makeText(getActivity(), "Account Fragment", Toast.LENGTH_SHORT).show();
@@ -46,29 +53,71 @@ public class AccountFragment extends Fragment {
     }
 
     private void loadUserDetails() {
-        binding.usernameAcctFragment.setText(preferenceManager.getString(Constants.KEY_NAME));
+        binding.userNameFragment.setText(preferenceManager.getString(Constants.KEY_NAME));
         ((MainActivity)getActivity()).currentUser.get()
                 .addOnCompleteListener(task -> {
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    binding.emailAcctFragment.setText(documentSnapshot.getString(Constants.KEY_EMAIL));
-                    binding.passwordAcctFragment.setText(documentSnapshot.getString(Constants.KEY_PASSWORD));
+                    documentSnapshot = task.getResult();
+                    binding.emailFragment.setText(documentSnapshot.getString(Constants.KEY_EMAIL));
+                    binding.passwordFragment.setText(documentSnapshot.getString(Constants.KEY_PASSWORD));
                 })
-                .addOnFailureListener(e -> binding.emailAcctFragment.setText("Unable to get user information."));;
+                .addOnFailureListener(e -> binding.emailFragment.setText("Unable to get user information."));;
         byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        binding.imageAcctFragment.setImageBitmap(bitmap);
+        binding.imageFragment.setImageBitmap(bitmap);
     }
 
     private void setListeners() {
-        binding.passwordShowAcctFragment.setOnClickListener(v -> {
-            if (binding.passwordAcctFragment.getInputType() != InputType.TYPE_CLASS_TEXT) {
-                binding.passwordAcctFragment.setInputType(InputType.TYPE_CLASS_TEXT );
+        binding.passwordFragment.setOnClickListener(v -> {
+            if (binding.passwordFragment.getInputType() != InputType.TYPE_CLASS_TEXT) {
+                binding.passwordFragment.setInputType(InputType.TYPE_CLASS_TEXT );
             } else {
-                binding.passwordAcctFragment.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                binding.passwordFragment.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
             }
         });
-        binding.buttonSignOutAcctFragment.setOnClickListener(v -> ((MainActivity)getActivity()).signOut());
+        binding.buttonSignOutFragment.setOnClickListener(v -> ((MainActivity)getActivity()).signOut());
+        binding.updatePasswordFragment.setOnClickListener(v -> {
+            if (isValidUpdatePassword()){
+                builder.setTitle("Update Password").setMessage("Are you sure?")
+                        .setPositiveButton("Ok", (dialogInterface, i) -> {
+                            updateAccount();
+                        })
+                        .setNegativeButton("Cancel", null);
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+        binding.deleteAccountFragment.setOnClickListener(v -> {
+            builder.setTitle("Delete Account").setMessage("Are you sure?")
+                    .setPositiveButton("Ok", (dialogInterface, i) -> {
+                        deleteAccount();
+                    })
+                    .setNegativeButton("Cancel", null);
+            AlertDialog alert = builder.create();
+            alert.show();
+        });
     }
 
+    private void updateAccount() {
+
+    }
+
+    private void deleteAccount() {
+
+    }
+
+    private boolean isValidUpdatePassword() {
+        if (binding.passwordFragment.getText().toString().trim().isEmpty()) {
+            showToast("Password is empty");
+            return false;
+        } else if (binding.passwordFragment.getText().toString().equals(documentSnapshot.getString(Constants.KEY_PASSWORD))) {
+            showToast("Password not change");
+            return false;
+        }
+        return true;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
 
 }
