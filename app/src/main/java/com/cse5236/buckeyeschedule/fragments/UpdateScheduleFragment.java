793a5030f42b542c8b54fb5ad69edbd52ddc791b
Linkda52 <linhongda77@gmail.com>
2022-10-31 02:57:14 -0400

    temp update, will not use

---
+++ app/src/main/java/com/cse5236/buckeyeschedule/fragments/UpdateScheduleFragment.java
@@ -0,0 +1,21 @@
+package com.cse5236.buckeyeschedule.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cse5236.buckeyeschedule.R;
+
+public class UpdateScheduleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_schedule, container, false);
+    }
+}
